bd17f2f04ea66037cd2de4fe8ec9d444d3e46402
lucas <lucaslopez2014ll@gmail.com>
2025-11-11 11:31:18 -0300

    Arreglo errores

--- test/test_ParBroczkoski.py
+++ test/test_ParBroczkoski.py
@@ -6,5 +6,5 @@ def es_par(n):
 #tests/test_es_par.py
 from funciones.es_par import es_par
 def test_es_par():
-assert es_par(4) is True
- assert es_par(7) is False
+    assert es_par(4) is True
+    assert es_par(7) is False
